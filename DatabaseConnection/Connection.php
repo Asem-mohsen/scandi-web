7a838d61d1fb934023ef9363653913f0b364efda
asem mohsen <75034372+Asem-mohsen@users.noreply.github.com>
2022-08-28 17:48:55 +0200

    Delete DatabaseConnection directory

--- DatabaseConnection/Connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername= "localhost";
-$username = "root";
-$password="";
-$databaseName="ScandiWeb";
-
-$Connection = mysqli_connect($servername , $username , $password , $databaseName );
-?>
